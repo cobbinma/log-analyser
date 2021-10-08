15cd67e75336d83cd6e7789566f846f19cb07807
matthew.cobbing <cobbinma@gmail.com>
2021-10-08 14:20:39 +0100

    context

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crate::models::Log;
-use anyhow::Error;
+use anyhow::{Context, Error};
 use std::{
     convert::TryFrom,
     fs::File,
@@ -11,7 +11,13 @@ mod models;
 fn main() -> Result<(), Error> {
     let file = File::open("input.log")?;
     for line in io::BufReader::new(file).lines() {
-        println!("{:?}", line.map_err(From::from).and_then(Log::try_from));
+        println!(
+            "{:?}",
+            line.context("unable to read line")
+                .map_err(From::from)
+                .and_then(Log::try_from)
+                .context("unable to parse line")
+        );
     }
 
     Ok(())
